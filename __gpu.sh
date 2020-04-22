2219ab3616dcd4e503b115377e84eb4cab5fd7ad
Bill Hogan <whogan@admins-MacBook-Pro-4.local>
2020-04-22 15:41:45 -0700

    remove cmi loc

--- __gpu.sh
+++ __gpu.sh
@@ -12,9 +12,7 @@
 #SBATCH -e /projects/ibm_aihl/whogan/acronym_resolution/out/ar_slurm_progress.err
 
 # Manual env setup
-export AR_MODEL_ROOT=pwd
 export LD_PRELOAD=/home/whogan/.conda/envs/latest_python/lib/libstdc++.so.6.0.26
-export CMI_MONGO_URI='mongodb://admin:AQTIOGCRVSHQASOS@portal-ssl178-46.bmix-dal-yp-d8bbba24-8b33-45c4-83f2-ec5cec265a60.2473981325.composedb.com:17476/compose?authSource=admin&ssl=true&retryWrites=false'
 
 source /opt/miniconda3/bin/activate latest_python
 
